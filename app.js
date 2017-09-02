a1238679cacbfb12309e8845f6e5da2f793daa18
Jiaping Zeng <jzeng@outlook.com>
2017-09-01 22:20:32 -0700

    asd

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var config = require('config')
 var bodyParser = require('body-parser')
 var request = require('request')
+var moment = require('moment')
 
 var app = express()
 var port = process.env.PORT || 3000
@@ -21,3 +22,115 @@ app.get('/webhook', (req, res) => {
         res.sendStatus(403)
     }
 })
+
+app.post('/webhook', (req, res) => {
+    var data = req.body
+    if (data.object === 'page') {
+        data.entry.forEach((entry) => {
+            entry.messaging.forEach((event) => {
+                if (event.message) {
+                    receivedMessage(event)
+                } else if (event.postback) {
+                    receivedPayload(event)
+                } else {
+                    console.log('received unknown event: ' + event)
+                }
+            })
+        })
+        res.status(200).send()
+    }
+})
+
+var receivedMessage = (event) => {
+    var senderId = event.sender.id
+    var message = event.message
+    var messageText = message.text
+    if(messageText) {
+        console.log('received message "' + messageText + '" from ' + senderId)
+        switch(messageText) {
+            default:
+                sendGenericMessage(senderId)
+        }
+    }
+}
+
+var receivedPayload = (event) => {
+    var senderId = event.sender.Id
+    var payload = event.postback.payload
+    switch(payload) {
+        case 'SCHEDULE_TODAY':
+            sendTextMessage(senderId, getSchedule(moment.format()))
+            break
+        case 'SCHEDULE_TOMORROW':
+            sendTextMessage(senderId, getSchedule(moment.add(1, 'days').format()))
+            break
+    }
+}
+
+var getSchedule = (time) => {
+    var regular = true
+    var day = moment(time).format('dddd')
+    if (regular) {
+        switch(day) {
+            case 'Saturday':
+            case 'Sunday':
+                return 'There\'s no school, silly'
+                break
+            default:
+                return 'It\'s a regular schedule day!'
+        }
+    }    
+}
+
+var sendGenericMessage = (recipientId) => {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: 'template',
+                payload: {
+                    template_type: 'button',
+                    text: 'What would you like to do?',
+                    buttons: [
+                        {
+                            type: 'postback',
+                            title: 'View today\'s schedule',
+                            payload: 'SCHEDULE_TODAY'
+                        }, {
+                            type: 'postback',
+                            title: 'View tomorrow\'s schedule',
+                            payload: 'SCHEDULE_TOMORROW'
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    console.log(messageData)
+    //callSendAPI(messageData)
+}
+
+var sendTextMessage = (recipientId, messageText) => {
+    var messageData = {
+        recipient: { id: recipientId },
+        message: { text: messageText }
+    }
+    console.log(messageData)
+    //callSendAPI(messageData)
+}
+
+var callSendAPI = (messageData) => {
+    request({
+        uri: apiUrl,
+        qs: { access_token: pageAccessToken },
+        method: 'POST',
+        json: messageData
+    }, (err, res, body) => {
+        if (!err && res.statusCode == 200) {
+            var recipientId = body.recipient_id
+            var messageId = body.message_id
+        }
+    })
+}
