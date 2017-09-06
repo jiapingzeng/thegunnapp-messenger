a1bc69eedecf0c447596c9fd0600da81b7a3d7a0
Jiaping Zeng <jiaping@gisar.me>
2017-09-05 21:30:16 -0700

    Commits from now on are unnecessary features that might break the app

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ var config = require('config')
 var bodyParser = require('body-parser')
 var request = require('request')
 var moment = require('moment-timezone')
+var path = require('path')
+var url = require('url')
 
 var app = express()
 var port = process.env.PORT || 3000
 app.listen(port)
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+var appUrl = process.env.APP_URL ? process.env.APP_URL : config.get('appUrl')
 var appSecret = process.env.MESSENGER_APP_SECRET ? process.env.MESSENGER_APP_SECRET : config.get('appSecret')
 var validationToken = process.env.MESSENGER_VALIDATION_TOKEN ? (process.env.MESSENGER_VALIDATION_TOKEN) : config.get('validationToken')
 var pageAccessToken = process.env.MESSENGER_PAGE_ACCESS_TOKEN ? (process.env.MESSENGER_PAGE_ACCESS_TOKEN) : config.get('pageAccessToken')
@@ -18,7 +22,13 @@ var googleApiKey = process.env.GOOGLE_API_KEY ? process.env.GOOGLE_API_KEY : con
 var calendarId = process.env.GOOGLE_CALENDAR_ID ? process.env.GOOGLE_CALENDAR_ID : config.get('calendarId')
 var calendarUrl = process.env.GOOGLE_CALENDAR_API_URL ? process.env.GOOGLE_CALENDAR_API_URL : config.get('calendarUrl')
 
+console.log('server started')
+
 var regularSchedule = require('./schedule')
+
+app.get('/', (req, res) => {
+    res.redirect('https://www.facebook.com/TheGunnApp-510499992673080/')
+})
 
 app.get('/webhook', (req, res) => {
     if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === validationToken) {
@@ -53,8 +63,19 @@ var receivedMessage = (event) => {
     if (messageText) {
         console.log('received message "' + messageText + '" from ' + senderId)
         var match = messageText.toLowerCase()
+        if (match == 'monday' || match == 'tuesday' || match == 'wednesday' || match == 'thursday' || match == 'friday') {
+            sendTextMessage(senderId, 'A regular' + capFirstLetter(match) + 'schedule looks like: \n' + getRegularSchedule(match))
+        } else if (match == 'download') {
+            sendDownloadLinks(senderId)
+        } else if (match == 'map') {
+            console.log(appUrl + 'campusmap.jpg')
+            sendImage(senderId, appUrl + 'campusmap.jpg')
+        } else {
+            sendGenericMessage(senderId)
+        }
         // this is terrible but im too dum to fix
-        if (match.includes('monday')) {
+        // b gone thot
+        /*if (match.includes('monday')) {
             sendTextMessage(senderId, 'A regular Monday schedule looks like: \n' + getRegularSchedule('Monday'))
         } else if (match.includes('tuesday')) {
             sendTextMessage(senderId, 'A regular Tuesday schedule looks like: \n' + getRegularSchedule('Tuesday'))
@@ -64,9 +85,7 @@ var receivedMessage = (event) => {
             sendTextMessage(senderId, 'A regular Thursday schedule looks like: \n' + getRegularSchedule('Thursday'))
         } else if (match.includes('friday')) {
             sendTextMessage(senderId, 'A regular Friday schedule looks like: \n' + getRegularSchedule('Friday'))
-        } else {
-            sendGenericMessage(senderId)
-        }
+        }*/
     }
 }
 
@@ -79,6 +98,9 @@ var receivedPayload = (event) => {
             break
         case 'SCHEDULE_TOMORROW':
             getSchedule(senderId, moment().add(1, 'days').format())
+            break
+        case 'DOWNLOAD':
+            sendDownloadLinks(senderId)
             break
     }
 }
@@ -102,20 +124,27 @@ var getSchedule = (senderId, time) => {
 }
 
 var getRegularSchedule = (day) => {
-    return regularSchedule[day]
+    return regularSchedule[capFirstLetter(day)]
 }
 
-var sendGenericMessage = (recipientId) => {
+var sendTextMessage = (recipientId, messageText) => {
     var messageData = {
-        recipient: {
-            id: recipientId
-        },
+        recipient: { id: recipientId },
+        message: { text: messageText }
+    }
+    console.log(messageData)
+    callSendApi(messageData)
+}
+
+var sendGenericMessage = (recipientId, text) => {
+    var messageData = {
+        recipient: { id: recipientId },
         message: {
             attachment: {
                 type: 'template',
                 payload: {
                     template_type: 'button',
-                    text: 'What would you like to see?',
+                    text: text ? text : 'What would you like to see?',
                     buttons: [
                         {
                             type: 'postback',
@@ -125,6 +154,10 @@ var sendGenericMessage = (recipientId) => {
                             type: 'postback',
                             title: 'Tomorrow\'s schedule',
                             payload: 'SCHEDULE_TOMORROW'
+                        }, {
+                            type: 'postback',
+                            title: 'Download TheGunnApp',
+                            payload: 'DOWNLOAD'
                         }
                     ]
                 }
@@ -135,10 +168,44 @@ var sendGenericMessage = (recipientId) => {
     callSendApi(messageData)
 }
 
-var sendTextMessage = (recipientId, messageText) => {
+var sendDownloadLinks = (recipientId, text) => {
     var messageData = {
         recipient: { id: recipientId },
-        message: { text: messageText }
+        message: {
+            attachment: {
+                type: 'template',
+                payload: {
+                    template_type: 'button',
+                    text: text ? text : 'Which one would you like to download?',
+                    buttons: [
+                        {
+                            type: 'web_url',
+                            url: 'https://itunes.apple.com/app/thegunnapp/id1141159201',
+                            title: 'iOS'
+                        },
+                        {
+                            type: 'web_url',
+                            url: 'https://play.google.com/store/apps/details?id=xyz.dchen.thegunnapp',
+                            title: 'Android'
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    console.log(messageData)
+    callSendApi(messageData)
+}
+
+var sendImage = (recipientId, image) => {
+    var messageData = {
+        recipient: { id: recipientId },
+        message: {
+            attachment: {
+                type: 'image',
+                payload: { url: image }
+            }
+        }
     }
     console.log(messageData)
     callSendApi(messageData)
@@ -185,4 +252,8 @@ var callCalendarApi = (time, cb) => {
             }
         }
     })
+}
+
+var capFirstLetter = (s) => {
+    return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase()
 }
